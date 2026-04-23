753d70ddfaff5a29393642104572de705cbb3858
Nicole Liecheski <nicole.liecheski@nubank.com.br>
2026-04-23 12:32:24 -0300

    modifying consumer

--- consumer.py
+++ consumer.py
@@ -1,6 +1,14 @@
-from kafka import KafkaConsumer
+from kafka import KafkaConsumer, KafkaProducer
 from const import *
 import sys
+
+try:
+    topic = sys.argv[1]
+except:
+    print('Usage: python3 consumer <input_topic> [output_topic]')
+    exit(1)
+
+output_topic = sys.argv[2] if len(sys.argv) > 2 else None
 
 # Create consumer: Option 1 -- only consume new events
 consumer = KafkaConsumer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT])
@@ -8,12 +16,14 @@ consumer = KafkaConsumer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT])
 # Create consumer: Option 2 -- consume old events (uncomment to test -- and comment Option 1 above)
 #consumer = KafkaConsumer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT], auto_offset_reset='earliest')
 
-try:
-  topic = sys.argv[1]
-except:
-  print ('Usage: python3 consumer <topic_name>')
-  exit(1)
-  
+if output_topic:
+    producer = KafkaProducer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT])
+    print(f'Bridging {topic} --> {output_topic}')
+
 consumer.subscribe([topic])
 for msg in consumer:
-    print (msg.value)
+    print(msg.value)
+    if output_topic:
+        processed = b'[processed] ' + msg.value
+        producer.send(output_topic, value=processed)
+        producer.flush()
